469d1e5c3aec707c40dcf0c78abe0ff100e21036
neliubin.egor <neo.8318731@gmail.com>
2023-08-03 16:39:25 +0300

    add vid1

--- _drafts/3_datamart.sql
+++ _drafts/3_datamart.sql
@@ -16,3 +16,42 @@ select
 	
 from emp.rel_emp_ent ree
 inner join emp.employment ent on ent.ent_id = ree.ent_id
+-----------------------------------------------------------------------------------
+with cal1 as (
+select
+    '2023-01-01'::date + i as f1,
+	left(cast('2023-01-01'::date + i as varchar(10)),7)::varchar(7) as f2
+from generate_series(0, current_date - '2023-01-01'::date - 1) i
+),
+cal2 as (
+select 
+	f2 as YM,
+	max(f1) as DATE_OTH
+from cal1
+group by f2
+),
+emp_ent as (
+select
+	ree.emp_id,
+	ree.ent_id,
+	ent.emp_startdate as ent_startdate,
+	ent.emp_enddate as ent_enddate
+from emp.rel_emp_ent ree
+inner join emp.employment ent on ent.ent_id = ree.ent_id
+),
+s1 as (
+select 
+* 
+from cal2 c2
+left join emp_ent ee on c2.DATE_OTH between ee.ent_startdate and ee.ent_enddate
+),
+vid1 as (
+select 
+	date_oth,
+	count(emp_id) as EMP_COUNT_FACT
+from s1
+group by date_oth
+order by 1 desc
+) 
+select * from vid1
+-----------------------------------------------------------------------------------
